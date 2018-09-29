67a7a87f5d4b60e4acb2c01eef4c5d4d5cda6554
ckuras <kuras.chris@gmail.com>
2018-09-29 15:04:38 -0400

    added players and player controller

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float move;
+    public float speed = 8.0f;
+
+    void Start ()
+    { 
+        if (gameObject.tag == "p1")
+        {
+            gameObject.transform.Translate(-1.0f, 0.0f, 0.0f);
+        } else
+        {
+            gameObject.transform.Translate(1.0f, 0.0f, 0.0f);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        //change input based on player
+        if (gameObject.tag == "p1")
+        {
+            move = Input.GetAxis("Player1");
+        } else
+        {
+            move = Input.GetAxis("Player2");
+        }
+
+        float translation = move * speed;
+        translation *= Time.deltaTime;
+        transform.Translate(translation, 0, 0);
+
+          
+    }
+}
